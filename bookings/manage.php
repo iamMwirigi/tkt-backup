3dc59d2fc9009b5cd529f999e7b55c6fa67d0e92
iammwirigi <mwirigimburugu81@gmail.com>
2025-06-12 08:55:41 +0300

    bookings

--- bookings/manage.php
+++ bookings/manage.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../config/database.php';
-require_once '../config/auth.php';
+require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../auth/auth.php';
 
 // Get request method
 $method = $_SERVER['REQUEST_METHOD'];

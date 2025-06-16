940a9e05b77070183b1d7bce7405c28a976d1097
iammwirigi <mwirigimburugu81@gmail.com>
2025-06-16 09:27:10 +0300

    fares

--- offenses/list.php
+++ offenses/list.php
@@ -4,8 +4,13 @@ require_once __DIR__ . '/../utils/functions.php';
 
 header('Content-Type: application/json');
 
-// Get company_id from GET parameters
+// Get company_id from either GET parameters or request body
 $company_id = $_GET['company_id'] ?? null;
+
+if (!$company_id) {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $company_id = $data['company_id'] ?? null;
+}
 
 // Validate company_id
 if (!$company_id) {

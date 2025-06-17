5c98470b725fc507fbf896ec9d02d9e26c59d588
iammwirigi <mwirigimburugu81@gmail.com>
2025-06-17 14:10:22 +0300

    vehicle configurations

--- vehicle-configurations/manage.php
+++ vehicle-configurations/manage.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../config/database.php';
-require_once '../utils/response.php';
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../utils/response.php';
 
 header('Content-Type: application/json');
 
